a3ca1823174c5f6fe0f66b843da2850b5c2a8680
felipe.paganin <felipepaganin97@gmail.com>
2023-03-31 13:26:34 -0300

    bernhoeft project

---
+++ Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using Bernhoeft.Shared.Entities;
+using System.Text.Json.Serialization;
+
+namespace Bernhoeft.Domain.Entities
+{
+    public class Category : Entity
+    {
+
+        public Category(string name)
+        {
+            Name = name;
+            Active = true;
+        }
+
+        public string? Name { get; set; }
+        public bool? Active { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Product>? Products { get; set; }
+
+        public void Put(Category category)
+        {
+            Name = category.Name;
+            Active = category.Active;
+        }
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using Bernhoeft.Shared.Entities;
+using System.Text.Json.Serialization;
+
+namespace Bernhoeft.Domain.Entities;
+
+public class Product : Entity
+{
+    protected Product() { }
+
+    public Product(string name, string description, decimal price, Guid categoryId)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+        CategoryId = categoryId;
+        Active= true;
+    }
+
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public decimal Price { get; private set; }
+    public bool? Active { get; private set; }
+    public Guid CategoryId { get; private set; }
+    [JsonIgnore]
+    public virtual Category? Category { get; private set; }
+
+    public void Put(Product product)
+    {
+        Name = product.Name;
+        Description = product.Description;
+        Price = product.Price;
+        CategoryId = product.CategoryId;
+    }
+}
